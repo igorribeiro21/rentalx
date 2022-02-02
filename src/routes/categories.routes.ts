c015c1c5baa19773aad0b0a87ec4f9eb1b89d1f6
igor.ribeiro <igorapalvesribeiro@gmail.com>
2022-02-01 21:26:34 -0300

    comecando a usar multer para importacao de arquivo

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,12 +1,19 @@
 import { Router, Request, Response } from 'express';
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
+import multer from 'multer';
+import { importCategoryController } from '../modules/cars/useCases/importCategory';
 
 const categoriesRoutes = Router();
+const upload = multer({
+    dest: "./tmp"
+});
 
 
-categoriesRoutes.post("/",(req,res) => createCategoryController.handle(req,res));
+categoriesRoutes.post("/", (req, res) => createCategoryController.handle(req, res));
 
-categoriesRoutes.get("/", (req: Request, res: Response) => listCategoriesController.handle(req,res));
+categoriesRoutes.get("/", (req, res) => listCategoriesController.handle(req, res));
+
+categoriesRoutes.post("/import", upload.single("file"), (req, res) => importCategoryController.handle(req,res));
 
 export { categoriesRoutes };
