b3501aae8672d2e637973df5d7f0fc1642ebc3e3
igor.ribeiro <igorapalvesribeiro@gmail.com>
2022-04-11 20:57:51 -0300

    comecando a criar estrutura para upload do avatar

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -25,6 +25,10 @@ export async function ensureAuthenticated(req: Request, res: Response, next: Nex
         if (!user) {
             throw new AppError("User does not exists!",401);
         }
+        
+        req.user = {
+            id: user_id
+        };
 
         next();
     } catch {
