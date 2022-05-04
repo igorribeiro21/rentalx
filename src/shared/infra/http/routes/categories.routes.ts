c51f265dcbcc100efe1aaba032c6a85a80452db4
igor.ribeiro <igorapalvesribeiro@gmail.com>
2022-05-03 21:40:36 -0300

    criando middleware para verificar se é Admin e usando

--- src/shared/infra/http/routes/categories.routes.ts
+++ src/shared/infra/http/routes/categories.routes.ts
@@ -3,6 +3,8 @@ import { ListCategoriesController } from '../../../../modules/cars/useCases/list
 import multer from 'multer';
 import { CreateCategoryController } from '../../../../modules/cars/useCases/createCategory/CreateCategoryController';
 import { ImportCategoryController } from '../../../../modules/cars/useCases/importCategory/ImportCategoryController';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
+import { ensureAdmin } from '../middlewares/ensureAdmin';
 
 const categoriesRoutes = Router();
 const upload = multer({
@@ -12,10 +14,10 @@ const createCategoryController = new CreateCategoryController();
 const importCategoryController = new ImportCategoryController();
 const listCategoriesController = new ListCategoriesController();
 
-categoriesRoutes.post("/", createCategoryController.handle);
+categoriesRoutes.post("/", ensureAuthenticated, ensureAdmin, createCategoryController.handle);
 
 categoriesRoutes.get("/", listCategoriesController.handle);
 
-categoriesRoutes.post("/import", upload.single("file"), importCategoryController.handle);
+categoriesRoutes.post("/import",ensureAuthenticated, ensureAdmin, upload.single("file"), importCategoryController.handle);
 
 export { categoriesRoutes };
