c44c3518a1d442a895d39be4912526aba1c323a8
igor.ribeiro <igorapalvesribeiro@gmail.com>
2022-05-21 11:52:59 -0300

    criando comparacao de 24 horas atraves de um provider

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,7 +1,7 @@
+import { IDateProvider } from '../../../../shared/container/providers/DateProvider/IDateProvider';
 import { AppError } from "../../../../shared/errors/AppError";
 import { Rental } from "../../infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "../../repositories/IRentalsRepository";
-
 interface IRequest {
     user_id: string;
     car_id: string;
@@ -10,12 +10,14 @@ interface IRequest {
 
 class CreateRentalUseCase {
     constructor(
-        private rentalsRepository: IRentalsRepository
+        private rentalsRepository: IRentalsRepository,
+        private dateProvider: IDateProvider
     ) {
 
     }
 
     async execute({ user_id, car_id, expected_return_date }: IRequest): Promise<Rental> {
+        const minimunHour = 24;
         const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(car_id);
 
         if (carUnavailable) {
@@ -26,6 +28,16 @@ class CreateRentalUseCase {
 
         if(rentalOpenToUser){
             throw new AppError("There's a rental in progress for user!");
+        }
+
+        const dateNow = this.dateProvider.dateNow();
+        const compare = this.dateProvider.compareInHours(            
+            dateNow,
+            expected_return_date
+        );
+
+        if(compare < minimunHour){
+            throw new AppError("Invalid return time!");
         }
 
         const rental = await this.rentalsRepository.create({
---
+++ src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
@@ -0,0 +1,25 @@
+import { IDateProvider } from "../IDateProvider";
+import dayjs from "dayjs";
+import uct from 'dayjs/plugin/utc';
+
+dayjs.extend(uct);
+
+class DayjsDateProvider  implements IDateProvider{
+
+    compareInHours(start_date: Date, end_date: Date): Number {
+        const end_date_utc = this.convertToUTC(end_date);
+        const start_date_uct = this.convertToUTC(start_date);
+
+        return dayjs(end_date_utc).diff(start_date_uct,"hours");
+    }
+
+    convertToUTC(date:Date): string{
+        return dayjs(date).utc().local().format();
+    }
+
+    dateNow(): Date {
+        return dayjs().toDate();
+    }
+}
+
+export {DayjsDateProvider}
