da31d15c95ec45890dd0e5a9e9f4a146ac0e64f1
igor.ribeiro <igorapalvesribeiro@gmail.com>
2022-06-01 21:59:42 -0300

    corrigindo os testes

--- src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
+++ src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
@@ -2,7 +2,7 @@ import { ICreateRentalDTO } from "../../dtos/ICreateRentalDTO";
 import { Rental } from "../../infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "../IRentalsRepository";
 
-class RentalsRepositoryInMemory implements IRentalsRepository {
+class RentalsRepositoryInMemory implements IRentalsRepository {    
     rentals: Rental[] = [];
 
     async findOpenRentalByCar(car_id: string): Promise<Rental> {
@@ -24,6 +24,13 @@ class RentalsRepositoryInMemory implements IRentalsRepository {
         this.rentals.push(rental);
 
         return rental;
+    }
+
+    async findById(id: string): Promise<Rental> {
+        return this.rentals.find(rental => rental.id === id);
+    }
+    async findByUser(user_id: string): Promise<Rental[]> {
+        return this.rentals.filter(rental => rental.user_id === user_id);
     }
 
 }
