2371f2ed8ac75105fe3f1be8ea1a83803d1e5122
igor.ribeiro <igorapalvesribeiro@gmail.com>
2022-07-12 22:48:42 -0300

    chapterVI - Adicionando Sentry

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -16,19 +16,36 @@ import { AppError } from '../../errors/AppError';
 import upload from "../../../config/upload";
 import rateLimiter from './middlewares/rateLimiter';
 
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
+
 createConnection();
 const app = express();
+
+Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [
+        new Sentry.Integrations.Http({ tracing: true }),
+        new Tracing.Integrations.Express({ app }),
+    ],
+    tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use(express.json());
 
 //app.use(rateLimiter);
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
-app.use("/avatar",express.static(`${upload.tmpFolder}/avatar`));
-app.use("/cars",express.static(`${upload.tmpFolder}/cars`));
+app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
+app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 app.use(cors());
 
 app.use(router);
+app.use(Sentry.Handlers.errorHandler());
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
     if (err instanceof AppError) {
